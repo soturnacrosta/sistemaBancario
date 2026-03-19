a2a7a8601482aa9042475428ac82f1d35aabc512
Mailton Lemos <lemos.dark@outlook.com>
2026-03-19 13:04:55 -0300

    refatoracao: guard clause || documentacao atualizada

--- sistema-bancario/src/main/java/com/github/soturnacrosta/Controle.java
+++ sistema-bancario/src/main/java/com/github/soturnacrosta/Controle.java
@@ -48,6 +48,8 @@ public class Controle { // responsável pelos menus de contato ao usuário
 
                             System.out.println();
 
+                            loginCliente = FormatadorCpf.formatarCpf(loginCliente);
+
                             for (Usuario u : Gerente.usuarios) { // para comparar a senha passada com a senha de um usuário, precisa percorrer a lista. Isso serve para não criar uma instância do objeto
                                 // Valida CPF e Senha
                                 if (u.getCpf().equals(loginCliente) && u.getSenha().equals(senhaCliente)) {
@@ -339,7 +341,7 @@ public class Controle { // responsável pelos menus de contato ao usuário
                     
                     if (contaDestino != null) { // Se a conta foi encontrada na lista global
                     
-                        if (!contaDestino.getConta().equals(usuarioAutenticado.getContaBancaria().getConta())) { // compara
+                        if (!contaDestino.getConta().equals(usuarioAutenticado.getContaBancaria().getConta())) { // compara os numeros das contas
 
                             System.out.println("Digito o valor:");
                                 double ted = input.nextDouble();
@@ -355,7 +357,7 @@ public class Controle { // responsável pelos menus de contato ao usuário
                         else {
 
                             System.out.println();
-                            System.out.println("Erro! A conta de destino não deve ser a conta remetente.");
+                            System.out.println("Erro! A conta de destino não deve ser a conta remetente."); // se for a mesma conta
                             System.out.println();
 
                         }
@@ -466,67 +468,70 @@ public class Controle { // responsável pelos menus de contato ao usuário
 
             while (!cpfAceito) {
 
-                System.out.println("Digite o CPF do cliente para encerrar conta:");
+                System.out.println("Digite o CPF do cliente para encerrar conta. Digite '0' caso queira cancelar:");
                 String cpfDeletar = input.nextLine();
 
-                if (FormatadorCpf.isFormatoValido(cpfDeletar)) {
 
-                    cpfDeletar = FormatadorCpf.formatarCpf(cpfDeletar); // DEVE Formatar a visualização para encaixar com o cpf da conta aberta
+                if (cpfDeletar.equals("0")) { // encerra o laço caso o usuário decida
 
-                    Usuario usuarioEncontrado = null;
-                
-                    for (Usuario u : Gerente.usuarios) {
+                    System.out.println();
+                    System.out.println("Voltando...");
+                    System.out.println();
 
-                        if (u.getCpf().equals(cpfDeletar)) {
-
-                            usuarioEncontrado = u;
-
-                            break;
-
-                        }
-
-                    }
-
-                    if (usuarioEncontrado != null) {
-
-                        System.out.println("Digite a senha:");
-                        String senhaFecharConta = input.nextLine();
-
-                            if (usuarioEncontrado.getSenha().equals(senhaFecharConta)) {
-
-                                gerente.fecharConta(cpfDeletar);
-
-                                cpfAceito = true;
-
-                            }
-
-                            else {
-
-                                System.out.println();
-                                System.out.println("Ops! Senha incorreta. Tente novamente.");
-
-                            }
-
-                     }
-
-                    else { // se não houver cadastro, não há conta para fechar
-
-                        System.out.println();
-                        System.out.println("Usuário não encontrado para esse CPF! Tente novamente.");
-                        System.out.println();
-
-                    }
-            
+                    break;
 
                 }
 
-                else {
+                if (!FormatadorCpf.isFormatoValido(cpfDeletar)) { // verifica o formato do cpf
 
                     System.out.println("\nErro! Formato inválido. O CPF deve conter 11 números.\n");
 
-                    break;
-                
+                    continue;
+
                 }
+
+                cpfDeletar = FormatadorCpf.formatarCpf(cpfDeletar); // DEVE Formatar a visualização para encaixar com o cpf da conta aberta
+
+                Usuario usuarioEncontrado = null;
+                
+                for (Usuario u : Gerente.usuarios) {
+
+                    if (u.getCpf().equals(cpfDeletar)) {
+
+                        usuarioEncontrado = u;
+
+                        break;
+
+                    }
+
+                }
+
+                if (usuarioEncontrado == null) { // verifica se existe uma conta com o cpf
+
+                    System.out.println();
+                    System.out.println("Usuário não encontrado para esse CPF! Tente novamente.");
+                    System.out.println();
+
+                    continue;
+
+                }
+
+                System.out.println("Digite a senha:"); // verifica a senha
+                    String senhaFecharConta = input.nextLine();
+                       
+                if (!usuarioEncontrado.getSenha().equals(senhaFecharConta)) {
+
+                    System.out.println();
+                    System.out.println("Ops! Senha incorreta. Tente novamente.");
+                    System.out.println();
+
+                    break;
+
+                }
+
+                gerente.fecharConta(cpfDeletar);
+
+                cpfAceito = true;                  
         
             }
            
@@ -541,76 +546,96 @@ public class Controle { // responsável pelos menus de contato ao usuário
 
             while (!cpfAceito) {
 
-                System.out.println("Digite o CPF do cliente para alterar dados da conta:");
+                System.out.println("Digite o CPF do cliente para alterar dados da conta. Digite '0' para cancelar:");
                     String alterarUsuarioCpf = input.nextLine();
 
-                if (FormatadorCpf.isFormatoValido(alterarUsuarioCpf)) {
+                if (alterarUsuarioCpf.equals("0")) { // encerra o laço caso o usuário decida
 
-                    alterarUsuarioCpf = FormatadorCpf.formatarCpf(alterarUsuarioCpf); // DEVE Formatar a visualização para encaixar com o cpf da conta aberta
-
-                    Usuario usuarioEncontrado = null;
-
-                    for (Usuario u : Gerente.usuarios) {
-
-                        if (u.getCpf().equals(alterarUsuarioCpf)) {
-
-                            usuarioEncontrado = u;
-
-                            break;
-
-                        }
-
-                    }
-
-                    if (usuarioEncontrado != null) {
-
-                        System.out.println("Usuário para CPF " + usuarioEncontrado.getCpf() + " encontrado. Digite a senha antiga do usuário:");
-                        String senhaAlt = input.nextLine();
-
-                        if (usuarioEncontrado.getSenha().equals(senhaAlt)) {
-
-                            System.out.println("Digite o novo nome:");
-                                String novoNome = input.nextLine();
-                            
-                            System.out.println("Digite a nova senha:");
-                                String novaSenha = input.nextLine();
-
-                            System.out.println("Digite o novo CPF:");
-                                String novoCpf = input.nextLine();
-
-                            gerente.alterarUsuario(alterarUsuarioCpf, novoNome, novaSenha,novoCpf);
-
-                            cpfAceito = true;
-
-                        }
-
-                        else {
-
-                            System.out.println();
-                            System.out.println("Ops! Senha incorreta. Tente novamente.");
-                            System.out.println();
-
-                        }
-
-                    }
-
-                    else {
-
-                        System.out.println();
-                        System.out.println("Usuário não encontrado para esse CPF! Tente novamente.");
-                        System.out.println();
-
-                    }
-
-                }
-
-                else {
-
-                    System.out.println("\nErro! Formato inválido. O CPF deve conter 11 números.\n");
+                    System.out.println();
+                    System.out.println("Voltando...");
+                    System.out.println();
 
                     break;
 
                 }
+
+                // primeira clausula de guarda
+                if (!FormatadorCpf.isFormatoValido(alterarUsuarioCpf)) { // verifica se o formato do cpf está valido
+
+                    System.out.println();
+                    System.out.println("Erro! Formato inválido. O CPF deve conter 11 números.");
+                    System.out.println();
+
+                    continue; // força o gerente a inserir um cpf válido na busca
+
+                }
+
+                alterarUsuarioCpf = FormatadorCpf.formatarCpf(alterarUsuarioCpf); // DEVE Formatar a visualização para encaixar com o cpf da conta aberta
+
+                Usuario usuarioEncontrado = null;
+
+                for (Usuario u : Gerente.usuarios) {
+
+                    if (u.getCpf().equals(alterarUsuarioCpf)) {
+
+                        usuarioEncontrado = u;
+
+                        break;
+
+                    }
+
+                } // faz a busca de cpf no banco de dados
+
+                // segunda clausula de guarda
+                if (usuarioEncontrado == null) { // caso não encontre um cpf no banco de dados
+
+                    System.out.println();
+                    System.out.println("Usuário não encontrado para esse CPF! Tente novamente.");
+                    System.out.println();
+
+                    continue; // força o gerente a procurar um cpf existente
+
+                }
+
+                System.out.println("Usuário para CPF " + usuarioEncontrado.getCpf() + " encontrado. Digite a senha antiga do usuário:");
+                    String senhaAlt = input.nextLine();
+
+                // terceira clausula de guarda
+                if (!usuarioEncontrado.getSenha().equals(senhaAlt)) { // verificação de senha
+
+                    System.out.println();
+                    System.out.println("Ops! Senha incorreta. Tente novamente.");
+                    System.out.println();
+
+                    break; // aborta inserção de senha inválida
+
+                }
+
+                System.out.println("Digite o novo nome:");
+                    String novoNome = input.nextLine();
+                
+                System.out.println("Digite a nova senha:");
+                    String novaSenha = input.nextLine();
+
+                System.out.println("Digite o novo CPF:");
+                    String novoCpf = input.nextLine();
+
+                if (!FormatadorCpf.isFormatoValido(novoCpf)) { // verifica o formato do novo cpf na hora da inserção
+
+                    System.out.println();
+                    System.out.println("Erro! Formato inválido. O CPF deve conter 11 números.");
+                    System.out.println("Voltando...");
+                    System.out.println();
+                    
+                    continue; //força nova senha válida
+
+                }
+
+                novoCpf = FormatadorCpf.formatarCpf(alterarUsuarioCpf); // DEVE Formatar a visualização para encaixar com o cpf da conta aberta
+
+                gerente.alterarUsuario(alterarUsuarioCpf, novoNome, novaSenha,novoCpf);
+
+                cpfAceito = true;
 
             }
 
